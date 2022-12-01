d519db6a4285b08fb74940d277151df4d70a2230
Dylanstiven27 <dilanstivenmuozsernad@gmail.com>
2022-12-01 17:29:30 -0500

    se añadieron las respuestas

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ cargarPregunta(0)
 
 function cargarPregunta(index){
     let objetoPregunta = base_de_preguntas[index]
+    let opciones=[...objetoPregunta.distractores]
+    opciones.push(objetoPregunta.respuesta)
     document.getElementById("pregunta").innerHTML = objetoPregunta.pregunta
+    document.getElementById("opcion-1").innerHTML = opciones[0]
+    document.getElementById("opcion-2").innerHTML = opciones[1]
+    document.getElementById("opcion-3").innerHTML = opciones[2]
+    document.getElementById("opcion-4").innerHTML = opciones[3]
+    document.getElementById("opcion-5").innerHTML = opciones[4]
 }
