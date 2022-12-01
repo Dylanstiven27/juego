800fd107e3f134a28063ae6d6d64631552fc3b73
Dylanstiven27 <dilanstivenmuozsernad@gmail.com>
2022-12-01 17:13:46 -0500

    Se dieron otros estilos y agregacion de un nuevo js, tambien se agregaron las preguntas

---
+++ base de preguntas.js
@@ -0,0 +1,8 @@
+let base_de_preguntas = [
+    {
+        pregunta: "¿como se llama el actual presidente de colombia?",
+        imagen: "../juego-jc/images/presidente.jpg",
+        respuesta:"gustavo petro",
+        distractores:["nicolas maduro","donal trump","michael jackson","james rodriguez"],
+    },
+];
