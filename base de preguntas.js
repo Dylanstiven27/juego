b1fcfdd4e5eb26add37870f12bbdd9ece82d533e
Dylanstiven27 <dilanstivenmuozsernad@gmail.com>
2022-12-01 21:47:39 -0500

    se agrego pregunta 4 y 5

--- base de preguntas.js
+++ base de preguntas.js
@@ -2,21 +2,35 @@ let base_de_preguntas = [
     {
         pregunta: "¿como se llama el actual presidente de colombia?",
         imagen: "../juego-jc/images/presidente.jpg",
-        respuesta:"gustavo petro",
-        distractores:["nicolas maduro","donal trump","michael jackson","james rodriguez"],
+        respuesta:"C gustavo petro",
+        distractores:["A nicolas maduro","D donal trump","E michael jackson","B james rodriguez"],
     },
 
     {
         pregunta: "¿cuantos mundiales tiene colombia?",
         imagen: "../juego-jc/images/mundiales.jpeg",
-        respuesta:"0",
-        distractores:["1","7","9","6"],
+        respuesta:"A 0",
+        distractores:["B 1","C 7","D 9","E 6"],
     },
 
     {
         pregunta: "jugador de colombia que juega en el liverpool",
         imagen: "../juego-jc/images/liverpool.jpg",
-        respuesta:"luis diaz",
-        distractores:["cristiano ronaldo","lionel messi"," falcao garcia","david ospina"],
+        respuesta:"C luis diaz",
+        distractores:["B cristiano ronaldo","A lionel messi","D falcao garcia","E david ospina"],
+    },
+
+    {
+        pregunta: "capital de colombia",
+        imagen: "../juego-jc/images/capital.jpg",
+        respuesta:"D bogota",
+        distractores:["B medellin","C new york","A cali","E noruega"],
+    },
+
+    {
+        pregunta: "¿que ciudad tiene tren en colombia?",
+        imagen: "../juego-jc/images/capital.jpg",
+        respuesta:"E medellin",
+        distractores:["D buenaventura","B cali","c barranquilla","A santander"],
     },
 ];
